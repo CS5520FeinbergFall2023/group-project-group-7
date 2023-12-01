4caca58969397a34b1e40070aabfa0ad2cab5efd
Michelle Zhang <michellezhang09876@gmail.com>
2023-11-30 23:30:34 -0500

    Resume add and search

--- JetpackComposeV1/app/src/main/java/edu/northeastern/jetpackcomposev1/models/resume/ResumeModel.kt
+++ JetpackComposeV1/app/src/main/java/edu/northeastern/jetpackcomposev1/models/resume/ResumeModel.kt
@@ -5,16 +5,17 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlinx.serialization.Serializable
-import java.time.ZonedDateTime
+import edu.northeastern.jetpackcomposev1.utility.getCurrentZonedDateTime
 import java.util.UUID
 
 @Serializable
 class ResumeModel(
     val id: String = UUID.randomUUID().toString(), // save your life, auto generate one, no need to get one from the firebase
-    val fileName: String = "",
-    val filePath: String = "",
-    val time: String = ZonedDateTime.now().toString(), // auto also
+    var fileName: String = "", // name of the pdf file
+    var filePath: String = "", // storage path
+    val time: String = getCurrentZonedDateTime(), // auto also
+    var activeStatus: Boolean = true // if delete the resume； it turns to false
 ) {
-    var nickName: String by mutableStateOf("")
+    var nickName: String by mutableStateOf("") // label
     var count: Int by mutableIntStateOf(0) // count how many applications use this resume
 }
